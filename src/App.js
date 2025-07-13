7ae020e9d8cbd8a5ab4b5e672d14e42f0257b12d
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-07-13 11:20:59 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>weather app</h1>
+    </div>
+  );
 }
-
-export default App;
