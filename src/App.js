20e964a385a9b6a370dce9ba63e2dd9fcde81197
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-07-14 08:39:00 +0430

    added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>weather app</h1>
+      <div className="container">
+        <h1>weather app</h1>
+        <footer>
+          This page was coded by{" "}
+          <a href="https://github.com/Mahdia-123" target="_blank">
+            Mahdia Khamoosh
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/Mahdia-123/react-project" target="_blank">
+            open-sourced o Github
+          </a>{" "}
+          hosted on{" "}
+          <a href="https://lovely-weather-search.netlify.app/ " target="_blank">
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
