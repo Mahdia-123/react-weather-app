355c5b982a10d1c52dfc6bbc1325a99283023f96
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-07-15 09:51:38 +0430

    added date on the weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import CurrentDate from "./CurrentDate";
 import axios from "axios";
+
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({});
   const [loaded, setLoaded] = useState(false);
@@ -8,12 +10,13 @@ export default function Weather(props) {
     setWeatherData({
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
-      date: "Monday, 8:00",
+      date: new Date(response.data.dt * 1000),
       humidity: response.data.main.humidity,
       city: response.data.name,
       iconUrl: `https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png`,
       description: response.data.weather[0].description,
     });
+
     setLoaded(true);
   }
   if (loaded) {
@@ -42,7 +45,9 @@ export default function Weather(props) {
         <h1>{weatherData.city}</h1>
 
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <CurrentDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
@@ -66,7 +71,7 @@ export default function Weather(props) {
             <ul>
               <li>Precipitation: 0%</li>
               <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind:{weatherData.wind} km/h</li>
+              <li>Wind: {weatherData.wind} km/h</li>
             </ul>
           </div>
         </div>
